95a77e088c35b440df2c9d4a5b280f954d76319e
Matt Cascardi <mattcascardi@gmail.com>
2023-06-26 21:49:30 +0000

    Updated plugin file description

--- gb-starter-block.php
+++ gb-starter-block.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name:       Gb Starter Block
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Gutenbergb Starter Block
+ * Description:       Gutenberg starter block template and example.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
